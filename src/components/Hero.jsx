8a738d7e8afdb2fce79218b2e5d25571e221cf64
Nikolay Putyata <nikolayputyata@gmail.com>
2025-12-28 18:16:39 +0200

    upd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,37 +2,43 @@
 
 function Hero() {
   return (
-    <section className="min-h-screen flex items-center justify-center px-6 text-center bg-white">
-  <div className="max-w-3xl mx-auto text-center">
-  {/* Заголовок */}
-  <h1
-    className="text-5xl md:text-6xl font-light leading-tight mb-6 text-gray-900"
-    style={{ fontFamily: "'DM Serif Text', serif" }}
-  >
-    Партнерське об'єднання{" "}
-    <span
-      className="text-yellow-500 font-semibold"
+    <section
+  className="min-h-screen flex items-center justify-center px-6 text-center relative"
+  style={{
+    backgroundImage: "url('/avt.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}
+>
+  {/* Overlay для затемнення */}
+  <div className="absolute inset-0 bg-black opacity-70"></div>
+
+  <div className="max-w-3xl mx-auto text-center relative z-10">
+    {/* Заголовок */}
+    <h1
+      className="text-5xl md:text-6xl font-light leading-tight mb-6 text-white"
       style={{ fontFamily: "'DM Serif Text', serif" }}
     >
-      Юнітраст
-    </span>
-  </h1>
+      Партнерське об'єднання{" "}
+      <span
+        className="text-yellow-500 font-semibold"
+        style={{ fontFamily: "'DM Serif Text', serif" }}
+      >
+        Юнітраст
+      </span>
+    </h1>
 
-  {/* Підзаголовок */}
-  <p
-    className="text-gray-600 text-xl max-w-xl mx-auto"
-    style={{ fontFamily: "'DM Serif Text', serif" }}
-  >
-    Ви не клієнт. Ви —{" "}
-    <span className="font-semibold italic gradient-text">
-      Партнер
-    </span>
-    .
-  </p>
-</div>
-
-
+    {/* Підзаголовок */}
+    <p
+      className="text-white text-xl max-w-xl mx-auto"
+      style={{ fontFamily: "'DM Serif Text', serif" }}
+    >
+      Ви не клієнт. Ви —{" "}
+      <span className="font-semibold italic gradient-text">Партнер</span>.
+    </p>
+  </div>
 </section>
+
 
 
   );
