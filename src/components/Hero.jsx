80dcf75e65dca8d1a62d7e264f6cc7fe2cb2c031
Nikolay Putyata <nikolayputyata@gmail.com>
2026-01-03 11:24:26 +0200

    upd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     relative">
       <div className="absolute inset-0 bg-white/10 z-0"></div>
 
-  <div className="relative z-10 max-w-3xl mx-auto text-start md:text-center">
+  <div className="relative z-10 max-w-3xl mx-auto text-start md:text-center mb-15 md:mb-0">
   {/* Заголовок */}
   <h1
     className="text-6xl font-light leading-tight mb-6 text-gray-900"
